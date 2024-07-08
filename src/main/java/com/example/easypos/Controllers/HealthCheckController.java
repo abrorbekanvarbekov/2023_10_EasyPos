c7b737757221de690dc69c5d7a3a3eada296b130
abrorbekanvarbekov <970213bu@gmail.com>
2024-07-08 19:44:26 +0900

    deploy: server first deploy

---
+++ src/main/java/com/example/easypos/Controllers/HealthCheckController.java
@@ -0,0 +1,37 @@
+package com.example.easypos.Controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+@RestController
+public class HealthCheckController {
+    @Value("${server.env}")
+    private String env;
+    @Value("${serverName}")
+    private String serverName;
+    @Value("${server.serverAddress}")
+    private String serverAddress;
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck(){
+        Map<String, String> responseData = new TreeMap<>();
+        responseData.put("env", env);
+        responseData.put("serverName", serverName);
+        responseData.put("serverAddress", serverAddress);
+        responseData.put("serverPort", serverPort);
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv(){
+        return ResponseEntity.ok(env);
+    }
+}
